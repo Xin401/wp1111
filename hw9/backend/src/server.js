28b16f1c6a3770612391059d61c8ef29644e8955
Xin401 <WenXin0401>
2022-12-26 19:30:05 +0800

    get

--- hw9/backend/src/server.js
+++ hw9/backend/src/server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
     });
 }
-// app.use(express.json());
+app.use(express.json());
 
 const port = process.env.PORT || 4000;
 app.listen(port, () =>
