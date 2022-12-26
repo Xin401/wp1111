7cffe23fb5210a855647293b21b31050be92117e
Xin401 <WenXin0401>
2022-12-26 18:56:40 +0800

    fuck

--- hw9/backend/src/server.js
+++ hw9/backend/src/server.js
@@ -2,14 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import db from './db';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
-const port = process.env.PORT || 4000;
 console.log(process.env.MONGO_URL);
 db.connect();
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+    app.use(cors());
+}
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+}
 app.use(express.json());
+app.use('/', routes);
+
+const port = process.env.PORT || 4000;
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`),
 );
-app.use('/', routes);
