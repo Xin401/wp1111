615b716596e8951ac1e1d3e41d3b2602f15b7413
Xin401 <WenXin0401>
2022-12-26 18:44:08 +0800

    fuck

--- hw9/backend/src/server.js
+++ hw9/backend/src/server.js
@@ -1,31 +1,15 @@
-import path from "path";
-
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
+import db from './db';
+import routes from './routes';
 
 const app = express();
-// init middleware
-if (process.env.NODE_ENV === "development") {
-    app.use(cors());
-}
-// define routes
-app.get("/api", (req, res) => {
-    // send the request back to the client
-    console.log("GET /api");
-    res.send({ message: "Hello from the server!" }).status(200);
-});
-
-if (process.env.NODE_ENV === "production") {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, "../frontend", "build")));
-    app.get("/*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-    });
-}
-
-// define server
 const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}.`);
-});
+console.log(process.env.MONGO_URL);
+db.connect();
+app.use(cors());
+app.use(express.json());
+app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`),
+);
+app.use('/', routes);
--- hw9/frontend/src/api.js
+++ hw9/frontend/src/api.js
@@ -1,14 +1,9 @@
 import axios from 'axios';
 
-// const instance = axios.create({
-//   baseURL: `http://localhost:4000/`,
-// });
+const instance = axios.create({
+  baseURL: `http://localhost:4000/`,
+});
 
-// export default instance;
-const API_ROOT =
-  process.env.NODE_ENV === "production"
-    ? "/api"
-    : "http://localhost:4000/api";
+export default instance;
 
-const api = axios.create({ baseURL: API_ROOT });
-export default api;
+// instance.get('/hi').then((data) => console.log(data));
