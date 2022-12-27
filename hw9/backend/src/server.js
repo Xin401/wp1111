1637fe6ef0be59865cbe77f91bcc376b24fc9e84
Xin401 <WenXin0401>
2022-12-27 14:17:34 +0800

    cors

--- hw9/backend/src/server.js
+++ hw9/backend/src/server.js
@@ -6,9 +6,7 @@ import routes from './routes';
 
 const app = express();
 db.connect();
-if (process.env.NODE_ENV === "development")
-    app.use(cors());
-
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
