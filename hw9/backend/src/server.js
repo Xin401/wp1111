96d24f2209d1f85fa4aed648f8664c69f0c6aca9
Xin401 <WenXin0401>
2022-12-27 12:54:31 +0800

    console log fuck

--- hw9/backend/src/server.js
+++ hw9/backend/src/server.js
@@ -17,7 +17,6 @@ if (process.env.NODE_ENV === "production") {
     app.get("/*", function (req, res) {
         res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
     });
-    console.log('fuck')
 }
 app.use(express.json());
 app.use('/api', routes);
